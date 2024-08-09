120612d6bb0294935ea3c007fe6d1d66b55bcbb1
alex <alexandersegerev@gmail.com>
2024-08-09 09:26:41 -0400

    updated the import

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { events } from './events.js';
+import { events } from './components/events.jsx';
 import { EventCard } from './components/EventCard.jsx';
 
 
